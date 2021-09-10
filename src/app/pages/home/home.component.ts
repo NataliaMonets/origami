6b70830ef0186d87b24e2dbf354d615cc124f407
Natalia Monets <natalia25.monets@gmail.com>
2021-09-10 19:03:28 +0300

    home page changed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product/product.model';
+import { OrderService } from 'src/app/shared/services/order/order.service';
+import { ProductService } from 'src/app/shared/services/product/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public userProduct: Array<IProduct> = [];
+  
+  constructor(
+    private productService: ProductService,
+    private orderService: OrderService
+  ) {
+   }
 
   ngOnInit(): void {
+    this.loadProducts();
   }
 
+  loadProducts(): void {
+    this.productService.get().subscribe(
+      data => {
+        this.userProduct = data;
+      }, err => {
+        console.log(err);
+      }
+    )
+  }
+
+  countProduct(product: IProduct, checker: boolean): void {
+    if (checker) {
+      product.count++;
+    } else {
+      if (product.count > 1) {
+        product.count--;
+      }
+    }
+  }
+
+  addToCart(product: IProduct): void {
+    this.orderService.addToCart(product);
+    product.count = 1;
+  }
 }
